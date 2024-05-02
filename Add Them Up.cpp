7ef9afab4e7b131e74fafe8eb5ba6ddd2f0439ef
Udipto <131182197+Udipto-Mondal@users.noreply.github.com>
2024-05-02 23:38:39 +0600

    Add files via upload

---
+++ Add Them Up.cpp
@@ -0,0 +1,16 @@
+/**
+ *    author:  Udipto
+ *    created:Thursday
+2 May 2024
+Dhaka, Bangladesh
+**/
+#include<bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+    int a,b;
+    cin>>a>>b;
+    int t=a+b;
+    cout<<t;
+    return 0;
+}
---
+++ Copycat.cpp
@@ -0,0 +1,14 @@
+/**
+ *    author:  Udipto
+ *    created:Thursday
+2 May 2024
+Dhaka, Bangladesh
+**/
+#include<bits/stdc++.h>
+using namespace std ;
+int main (){
+int n;
+cin>>n;
+cout<<n<<"\n";
+return 0;
+}
